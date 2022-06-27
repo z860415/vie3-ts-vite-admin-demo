3fef356db28a03871bcfbbe1b89be357149df15f
Eric_li <z860415@gmail.com>
2022-06-27 17:10:27 +0800

    新增插值語法練習

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,11 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div> {{ message ? '判斷為真' : '判斷為假'}}</div>
 </template>
 
+<script setup lang="ts">
+const message: number = 0
+
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
